1524d17986928b9d5d191ab52d7fe1d17bae5c39
Stephen Collins <stephenc211@gmail.com>
2020-08-27 19:41:14 -0500

    sets up test inputs for calculating residual dimensions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import PanelManager from './components/PanelManager'
 import './App.css';
 
@@ -16,15 +16,15 @@ import './App.css';
 // Panels will be sorted according of xOffset and yOffset (since all panel offsets are based from the same relative point)
 const DUMMY_PANEL_DATA = {
   PanelA: {
-    w: 100,
-    h: 100,
+    w: 0.5,
+    h: 0.5,
     xOffset: 0,
     yOffset: 0
   },
   PanelB: {
-    w: 100,
-    h: 100,
-    xOffset: 100,
+    w: 0.5,
+    h: 0.5,
+    xOffset: 0.5,
     yOffset: 0
   }
 }
@@ -33,9 +33,16 @@ const PanelA = () => (<div style={{background: 'blue', display: 'flex', width: '
 const PanelB = () => (<div style={{background: 'lightgreen', display: 'flex', width: '100%', height: '100%'}}>Panel B</div>)
 
 function App() {
-  const [panelId, setPanelId] = useState('')
+  const containerRef = useRef(null)
   const [panelData, setPanelData] = useState(DUMMY_PANEL_DATA)
-  window.test = PanelA
+
+  const onRangeChange = (name, event) => {
+    const targetName = event.target.name
+    const nextPercent = parseInt(event.target.value) / 100
+    const nextPanelData = Object.assign({}, panelData)
+    nextPanelData[name][targetName] = nextPercent
+    setPanelData(nextPanelData)
+  }
   return (
     <div className="App">
       <div
@@ -45,20 +52,30 @@ function App() {
           border: '1px solid lightgrey',
           maxWidth: 500,
           alignContent: 'left',
-          padding: '1em'
+          padding: '1em',
+          textAlign: 'left'
         }}
       >
+        <b>Panel A</b>
         <label>
-          Panel Id:<br/>
-          <input value={panelId} onChange={event => setPanelId(event.target.value)} />
+          height (0 - 1) {Math.round(panelData['PanelA'].h * 100)}%
+          <input type='range' min={0} max={100} name='h' value={panelData['PanelA'].h * 100} onChange={event => onRangeChange('PanelA', event)} />
+          <br/>
+          width (0 - 1) {Math.round(panelData['PanelA'].w * 100)}%
+          <input type='range' min={0} max={100} name='w' value={panelData['PanelA'].w * 100} onChange={event => onRangeChange('PanelA', event)} />
         </label>
-        <div style={{ maxWidth: 500}}>
-          <button>Maximize</button>
-          <button>Minimize</button>
-        </div>
+        <b>Panel B</b>
+        <label>
+          height (0 - 1) {Math.round(panelData['PanelB'].h * 100)}%
+          <input type='range' min={0} max={100} name='h' value={panelData['PanelB'].h * 100} onChange={event => onRangeChange('PanelB', event)} />
+          <br/>
+          width (0 - 1) {Math.round(panelData['PanelB'].w * 100)}%
+          <input type='range' min={0} max={100} name='w' value={panelData['PanelB'].w * 100} onChange={event => onRangeChange('PanelB', event)} />
+        </label>
       </div>
-      <div style={{border: '1px solid darkblue', maxWidth: 900, minWidth: 900, minHeight: 900, maxHeight: 900}}>
+      <div ref={containerRef} style={{border: '1px solid darkblue', maxWidth: 900, minWidth: 900, minHeight: 900, maxHeight: 900}}>
         <PanelManager
+          containerRef={containerRef}
           panelComponents={[PanelA, PanelB]}
           panelData={panelData}
 
--- src/components/PanelManager/index.js
+++ src/components/PanelManager/index.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Panel from '../Panel'
 
-const PanelManager = ({ panelComponents, panelData}) => {
+const PanelManager = ({ containerRef, panelComponents, panelData}) => {
+
+  const [mounted, setMounted] = useState(false)
   const onPanelSizeChange = (data, name) => {
     console.warn('onPanelSizeChange called', { data, name})
   }
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+  if (!mounted) {
+    return null
+  }
+  const { width, height, x, y } = containerRef.current.getBoundingClientRect() 
   return (
     <div style={{position: 'relative'}}>
       {
         panelComponents.map( PanelComponent => {
-          const panelProps = panelData[PanelComponent.name] 
-          console.log({ panelProps })
+          const {w, h, yOffset, xOffset } = panelData[PanelComponent.name] 
+          console.log({ w, h, yOffset, xOffset, width, height,x,  y})
           return (
             <Panel
               onPanelSizeChange={data => onPanelSizeChange(data, PanelComponent.name)}
               key={PanelComponent.name}
-              {...panelProps}
+              w={w * width}
+              h={h * height}
+              yOffset={yOffset * height}
+              xOffset={xOffset * width}
             >
               <PanelComponent />
             </Panel>
