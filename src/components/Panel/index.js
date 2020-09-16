4647595c3be6b52bab933a7335a42e01305f5b90
Stephen Collins <stephenc211@gmail.com>
2020-09-16 00:06:36 -0500

    rewrote drag handling to avoid Drag API pitfalls

--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -1,72 +1,20 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import { PanelSystemContext } from '../PanelManager'
 import './Panel.css'
 
-const getPercentChange = (previous, current) => ((current - previous) / previous * 100) / 100
+// NOTE: the Drag API cannot be used if I want to set a cursor while "dragging"
+// must use mouse move, down and up to simulate drag
+const Panel = ({ w, h, x, y, children, nodeId, }) => {
 
-// https://stackoverflow.com/questions/30725607/is-there-any-way-to-bind-a-click-event-to-a-divs-left-border-in-jquery
-const Panel = ({ w, h, x, y, children, nodeId }) => {
-  const [startPos, setStartPos] = useState({})
-
-  const [,updatePanelDataContext] = useContext(PanelSystemContext)
+  const [, { setDraggingNode, setStartPos }] = useContext(PanelSystemContext)
 
 
-  const onDragStart = event => {
+  const onMouseDown = (event, edge) => {
+    setDraggingNode({ nodeId, edge})
     setStartPos({ startX: event.pageX, startY: event.pageY })
-    let img = document.getElementById('drag-placeholder') 
-    if (!document.getElementById('drag-placeholder')) {
-      img = new Image()
-      img.id = 'drag-placeholder'
-      img.style.visibility = 'hidden'
-      img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
-      document.body.appendChild(img);
-      event.dataTransfer.setDragImage(img, 0, 0);
-    } else {
-      event.dataTransfer.setDragImage(img, 0, 0);
-    }
   }
-  const onDrag = (event, nodeId, edgeType) => {
-    event.preventDefault()
-    const { startX, startY } = startPos
-    if (edgeType === 'LE' || edgeType === 'RE') {
-      const changeX = edgeType === 'RE' ? getPercentChange(startX, event.pageX) : getPercentChange(event.pageX, startX)
-      if (isNaN(changeX) || changeX === 0) {
-        return
-      }
-      const changeEvent = {
-        nodeId,
-        edgeType,
-        data: {
-          w: changeX,
-          h: 0
-        }
-      }
-      updatePanelDataContext(changeEvent)
-      setStartPos({ startX: event.pageX, startY: event.pageY})
-      return
-    }
-    if (edgeType === 'TV' || edgeType === 'BV') {
-      const changeY = edgeType === 'BV' ? getPercentChange(startY, event.pageY) : getPercentChange(event.pageY, startY)  
-      if (isNaN(changeY) || changeY === 0) {
-        return
-      }
-      const changeEvent = {
-        nodeId,
-        edgeType,
-        data: {
-          w: 0,
-          h: changeY
-        }
-      }
-      updatePanelDataContext(changeEvent)
-      setStartPos({ startX: event.pageX, startY: event.pageY})
-      return
-    }
-  }
-  const onDragEnd = () => setStartPos({})
   return (
     <div
-      onDragOver={event => event.preventDefault()}
       style={{
         display: 'flex',
         position: 'absolute',
@@ -78,9 +26,7 @@ const Panel = ({ w, h, x, y, children, nodeId }) => {
         left: `${x * 100}%`,
       }}>
       <div
-        draggable="true"
-        onDragStart={onDragStart}
-        onDrag={event => onDrag(event, nodeId, 'LE')}
+        onMouseDown={event => onMouseDown(event, 'LE')}
         className='panel-horizontal-edge'
         style={{ border: x !== 0 ? '4px solid skyblue': 'none', display: x !== 1 ? '' : 'none'}}
       />
@@ -92,27 +38,19 @@ const Panel = ({ w, h, x, y, children, nodeId }) => {
           }}
         >
           <div
-            draggable="true"
-            onDragStart={onDragStart}
-            onDragEnd={onDragEnd}
-            onDrag={event => onDrag(event, nodeId, 'TV')}
+            onMouseDown={event => onMouseDown(event, 'TV')}
             className='panel-vertical-edge'
             style={{ border: y !== 0 ? '4px solid lime': 'none', display: y !== 0 ? '' : 'none'}}
           />
           {children}
           <div
-            draggable="true"
-            onDragStart={onDragStart}
-            onDragEnd={onDragEnd}
-            onDrag={event => onDrag(event, nodeId, 'BV')}
+            onMouseDown={event => onMouseDown(event, 'BV')}
             className='panel-vertical-edge'
             style={{ border: y === 0 && y + h !== 1 ? '4px solid black': 'none', display: y === 0 && y + h !== 0 ? '' : 'none'}}
           />
         </div>
       <div
-        draggable="true"
-        onDragStart={onDragStart}
-        onDrag={event => onDrag(event, nodeId, 'RE')}
+        onMouseDown={event => onMouseDown(event, 'RE')}
         className='panel-horizontal-edge'
         style={{ border: x + w !== 1 ? '4px solid magenta': 'none', display: x + w !== 1 ? '' : 'none'}}
       />
--- src/components/PanelManager/index.js
+++ src/components/PanelManager/index.js
@@ -3,10 +3,12 @@ import { updateGraph } from './PanelGraph'
 import Panel from '../Panel'
 
 export const PanelSystemContext = createContext()
-
+const getPercentChange = (previous, current) => ((current - previous) / previous * 100) / 100
 const PanelManager = ({ panelComponents, panelData, onPanelDataChange }) => {
 
   const [panelDataContext, setPanelDataContext] = useState(panelData)
+  const [draggingNode, setDraggingNode] = useState({})
+  const [startPos, setStartPos] = useState({})
 
   useEffect(() => {
     setPanelDataContext(panelData)
@@ -28,16 +30,68 @@ const PanelManager = ({ panelComponents, panelData, onPanelDataChange }) => {
       </Panel>
     )
   }
-  const panelManagerContext = [panelDataContext, updatePanelDataContext]
+  const panelManagerContext = [{panelDataContext, draggingNode}, { updatePanelDataContext, setDraggingNode, setStartPos }]
+  const onMouseMove = (event) => {
+    event.preventDefault()
+    const { nodeId, edge: edgeType } = draggingNode
+    if (nodeId === undefined) {
+      return
+    }
+    const { startX, startY } = startPos
+    if (edgeType === 'LE' || edgeType === 'RE') {
+      const changeX = edgeType === 'RE' ? getPercentChange(startX, event.pageX) : getPercentChange(event.pageX, startX)
+      if (isNaN(changeX) || changeX === 0) {
+        return
+      }
+      const changeEvent = {
+        nodeId,
+        edgeType,
+        data: {
+          w: changeX,
+          h: 0
+        }
+      }
+      updatePanelDataContext(changeEvent)
+      setStartPos({ startX: event.pageX, startY: event.pageY})
+      return
+    }
+    if (edgeType === 'TV' || edgeType === 'BV') {
+      const changeY = edgeType === 'BV' ? getPercentChange(startY, event.pageY) : getPercentChange(event.pageY, startY)  
+      if (isNaN(changeY) || changeY === 0) {
+        return
+      }
+      const changeEvent = {
+        nodeId,
+        edgeType,
+        data: {
+          w: 0,
+          h: changeY
+        }
+      }
+      updatePanelDataContext(changeEvent)
+      setStartPos({ startX: event.pageX, startY: event.pageY})
+      return
+    }
+  }
+  const onMouseUp = () => {
+    setDraggingNode({})
+    setStartPos({})
+  }
+  
   return (
     <PanelSystemContext.Provider value={panelManagerContext}>
       <div
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
         style={{
           display: 'flex',
           position: 'relative',
           background: 'yellow',
           height: '100%',
-          width: '100%'
+          width: '100%',
+          cursor: draggingNode && draggingNode.edge && (draggingNode.edge === 'LE' || draggingNode.edge === 'RE')
+          ? 'col-resize' 
+          : draggingNode && draggingNode.edge && (draggingNode.edge === 'TV' || draggingNode.edge === 'BV') ? 'row-resize' : ''
         }}
       >
         {
